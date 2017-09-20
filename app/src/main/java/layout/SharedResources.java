ee77b479456d4540d0ccd76a38a6526bc3356c9d
dhaneshkatre <katre.dhanesh26@gmail.com>
2017-09-20 21:23:54 +0530

    First Commit

---
+++ app/src/main/java/layout/SharedResources.java
@@ -0,0 +1,18 @@
+package layout;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by Patil on 25-08-2016.
+ */
+public class SharedResources {
+    public static final String URL="https://csipublicity-af5de.firebaseio.com/";
+    public static final String EVENT=URL+"events/";
+    public static final String USER=URL+"users/";
+    public static final String ENTRIES=URL+"entries/";
+    public static final String SharedUSERDATA="session";
+    public static final String SharedNAME="NAME";
+    public static final String SharedInital="INITAL";
+    public static final String SharedENTRIES="NOENTRIES";
+    public static final String ADMINNO="+918698108192";
+}
